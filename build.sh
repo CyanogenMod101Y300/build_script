aac3dbe55a3bfde03462c89a33d45f319ad7b4b7
luca020400 <luca.stefani.ge1@gmail.com>
2014-09-17 21:00:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 echo "Build script for CarbonRom 4.2.2 by luca020400"
 echo "Enjoy !!!"
+repo init -u git://github.com/luca020400/android.git -b jb2
 rm -rf out/target/product/u8833 >> /dev/null
 rm -rf system/core >> /dev/null
-repo sync -f && rm -rf system/core && repo sync -f
+repo sync -f && rm -rf system/core >> /dev/null && repo sync -f
 . build/envsetup.sh
 lunch <<EOF
 8
