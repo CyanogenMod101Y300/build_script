699febd63dce040cf1d6d367be2830c9b9055910
luca020400 <luca.stefani.ge1@gmail.com>
2014-10-05 18:11:03 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 CORES=$(( `nproc` + 1 ))
 
 echo "Build script for CarbonRom 4.2.2 by luca020400"
+echo ""
 echo "Enjoy !!!"
+echo ""
 
 echo "Cleaning"
 rm -rf out/target/product/u8833 >> /dev/null
@@ -11,13 +13,16 @@ rm -rf device/huawei/u8833 >> /dev/null
 rm -rf out/target/product/u8825 >> /dev/null
 rm -rf device/huawei/u8825 >> /dev/null
 rm -rf vendor/huawei/ >> /dev/null
+echo ""
 
 echo "Initializing the CarbonRomJellyBeanY300 Repository"
 repo init -u git://github.com/CarbonRomJellyBeanY300/android.git -b jb2 >> /dev/null
+echo ""
 
 echo "Downloading sources 1/2"
 rm -rf system/core >> /dev/null
 repo sync -f  >> /dev/null
+
 echo "Downloading sources 2/2"
 rm -rf system/core >> /dev/null
 repo sync -f  >> /dev/null
@@ -25,6 +30,7 @@ repo sync -f  >> /dev/null
 echo "Select your device"
 echo "Y300/G510 --> 1"
 echo "G330 --> 2"
+echo ""
 
 read -s -n 1 device
 if [[ $device = "" ]]; then
@@ -34,19 +40,26 @@ elif [[ $device = "1" ]]; then
     rm -rf device/huawei/u8825 >> /dev/null
     . build/envsetup.sh
     lunch carbon_u8833-userdebug
+    echo ""
     echo "Building CarbonRom 4.2.2 for Y300/G510"
     make carbon -j$CORES
+    echo ""
     echo "Done"
     cp out/target/product/u8833/CARBON*.zip . >> /dev/null
+    echo ""
+    echo "The build is in the carbon directory"
 elif [[ $device = "2" ]]; then
     echo "You selected G330"
     rm -rf device/huawei/u8833 >> /dev/null
     . build/envsetup.sh
     lunch carbon_u8825-userdebug
+    echo ""
     echo "Building CarbonRom 4.2.2 for G330"
     make carbon -j$CORES
+    echo ""
     echo "Done"
     cp out/target/product/u8825/CARBON*.zip . >> /dev/null
+    echo ""
+    echo "The build is in the carbon directory"
 else echo "You must select 1 or 2"
 fi
-echo "The build is in the carbon directory"
