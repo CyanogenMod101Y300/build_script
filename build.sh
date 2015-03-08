d2fbea2fc7be8e5e5a43dabe1fe69253bb32a786
luca020400 <luca.stefani.ge1@gmail.com>
2015-03-08 14:38:21 +0100

    Add CM 10.1 Support

--- build.sh
+++ build.sh
@@ -2,26 +2,24 @@
 
 CORES=$(( `nproc` + 1 ))
 
-echo "Build script for CarbonRom 4.2.2 by luca020400"
+echo "Build script for CyanogenMod 10.1 by luca020400"
 echo "Enjoy !!!"
 echo ""
 
 echo "Cleaning"
-rm -rf out/target/product/ >> /dev/null
+rm -rf out/target >> /dev/null
 rm -rf device/huawei/ >> /dev/null
 rm -rf vendor/huawei/ >> /dev/null
 echo ""
 
-echo "Initializing the CarbonRomJellyBeanY300 Repository"
-repo init -u git://github.com/CarbonRomJellyBeanY300/android.git -b jb2 >> /dev/null
+echo "Initializing the CyanogenMod Repository"
+repo init -u git://github.com/CyanogenMod/android.git -b cm-10.1 >> /dev/null
 echo ""
 
 echo "Downloading sources 1/2"
-rm -rf system/core >> /dev/null
 repo sync -f -j8 >> /dev/null
 
 echo "Downloading sources 2/2"
-rm -rf system/core >> /dev/null
 repo sync -f -j8 >> /dev/null
 
 echo "Y300/G510 --> 1"
@@ -37,12 +35,12 @@ elif [[ $device = "1" ]]; then
     echo "You selected Y300/G510"
     rm -rf device/huawei/u8825 >> /dev/null
     . build/envsetup.sh
-    lunch carbon_u8833-userdebug
+    breakfast u8833
     echo ""
-    echo "Building CarbonRom 4.2.2 for Y300/G510"
+    echo "Building CyanogenMod 10.1 for Y300/G510"
     make carbon -j$CORES
     echo "Done"
-    cp out/target/product/u8833/CARBON*.zip . >> /dev/null
+    cp out/target/product/u8833/cm-*.zip . >> /dev/null
     echo ""
     echo "The build is in the carbon directory"
 elif [[ $device = "2" ]]; then
@@ -50,12 +48,12 @@ elif [[ $device = "2" ]]; then
     echo "You selected G330"
     rm -rf device/huawei/u8833 >> /dev/null
     . build/envsetup.sh
-    lunch carbon_u8825-userdebug
+    breakfast u8825
     echo ""
-    echo "Building CarbonRom 4.2.2 for G330"
+    echo "Building CyanogenMod 10.1 for G330"
     make carbon -j$CORES
     echo "Done"
-    cp out/target/product/u8825/CARBON*.zip . >> /dev/null
+    cp out/target/product/u8825/cm-*.zip . >> /dev/null
     echo ""
     echo "The build is in the carbon directory"
 else echo "" && echo "You must select 1 or 2"
